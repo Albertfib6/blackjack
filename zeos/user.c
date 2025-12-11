51579f3498c51b0f63c5a51fc1f7461cab429157
Albert Paños Garnica <114769327+Albertfib6@users.noreply.github.com>
2025-12-11 15:20:35 +0100

    Update user.c

--- zeos/user.c
+++ zeos/user.c
@@ -68,29 +68,20 @@ void handler_teclado(char key, int pressed) {
     }
 }
 
-void generate_checkerboard_pattern(char *buffer) {
-    for (int y = 0; y < 25; y++) {
-        for (int x = 0; x < 80; x++) {
-            int pos = (y * 80 + x) * 2;
-
-            /* Row 0 is black for time/FPS display */
-            if (y == 0) {
-                buffer[pos] = ' ';
-                buffer[pos + 1] = 0x00;
-                continue;
-            }
-
-            /* Checkerboard pattern: alternate A/B both horizontally and vertically */
-            int is_alternate = (x + y) % 2;
-            buffer[pos] = is_alternate ? 'B' : 'A';       /* Character */
-            buffer[pos + 1] = is_alternate ? 0x4F : 0x1F; /* Red on white : Blue on white */
+void generar_pantalla(char *buffer) {
+    for (int i = 0; i < 25; i++) {
+        for (int j = 0; i < 80; j++) {
+            int pos = (i * 80 + j) * 2;
+            int par = pos%2;
+            buffer[pos] = par ? 'X' : 'Y';      
+            buffer[pos + 1] = par ? 0x2F : 0x4F; 
         }
     }
 }
 
 void test_screen() {
     char screen_buffer[80*25*2];
-    generate_checkerboard_pattern(screen_buffer);
+    generar_pantalla(screen_buffer);
     write(10, screen_buffer, sizeof(screen_buffer));
 }
 
@@ -172,6 +163,7 @@ int __attribute__ ((__section__(".text.main")))
     }
 }
  
+
 
 
 
